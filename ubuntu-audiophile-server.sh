89c150701b76ba84a4d725835ca9bb3e2ea79327
a <a>
2021-01-30 18:54:41 +0000

    update

--- ubuntu-audiophile-server.sh
+++ ubuntu-audiophile-server.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-# render selection
-echo    "************************"
-echo    "* Select upnp renderer *"
-echo    "************************"
-echo    "* 1. mpd upmpdcli      *"
-echo    "* 2. gmediarender      *"
-echo    "************************"
-echo -n "Select renderer (1/2)   "
-read ans
+# software selection
+echo -n "Install gmediarender (y/n)? "
+read gmediarender
 
-RENDER=mpd
-[ "$ans" == 2 ] && RENDER=gmediarender
-USERNAME=$(whoami)
+echo -n "Install mpd (y/n)? "
+read mpd
+
+echo -n "Install upmpdcli (upnp mpd integration, no seeking supported) (y/n)? "
+read upmpdcli
 
 # external repos
 sudo add-apt-repository -y ppa:jean-francois-dockes/upnpp1
@@ -26,25 +22,27 @@ sudo apt-get -y remove packageit snap update-manager-core
 
 # install system software
 sudo apt-get -y install linux-lowlatency linux-tools-lowlatency cpufrequtils
-sudo apt-get -y install openssh-server mc
+sudo apt-get -y install openssh-server mc net-tools
 sudo apt-get -y install alsa alsa-tools
 
-# install upnp renderer
-if [ $RENDER == "mpd" ]; then
-  sudo apt-get -y install mpd upmpdcli
-  sudo apt-get -y remove gmediarender gstreamer1.0-alsa gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-tools
-elif [ $RENDER == "gmediarender" ]; then
-  sudo apt-get -y remove mpd upmpdcli
-  sudo apt-get -y install gmediarender gstreamer1.0-alsa gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-tools
-fi
+# remove previous installations
+sudo apt-get -y remove mpd mpc
+sudo apt-get -y remove upmpdcli
+sudo apt-get -y remove gmediarender gstreamer1.0-alsa gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-tools
+
+# install software
+[ $mpd == "y" ] && sudo apt-get -y install mpd mpc
+[ $upmpdcli == "y" ] && sudo apt-get -y install upmpdcli
+[ $gmediarender == "y" ] && sudo apt-get -y install gmediarender gstreamer1.0-alsa gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-tools
 
 # install configurations
-sudo systemctl disable ondemand
-sudo adduser $USERNAME audio
-sudo cp -r etc/ /
-sudo sed -i "s/_USERNAME_/$USERNAME/g" /etc/default/gmediarender
-sudo sed -i "s/_RENDER_/$RENDER/g" /etc/systemd/system/rc-local.service
+USERNAME=$(whoami)
 mkdir -p /home/$USERNAME/Music
+sudo cp -r etc/ /
+sudo sed -i "s/_USERNAME_/$USERNAME/g" /etc/mpd.conf
+sudo systemctl disable ondemand
+sudo systemctl enable mpd-renice.service
+sudo systemctl enable gmediarender-renice.service
 
 # clean up
 sudo apt-get -y autoremove
