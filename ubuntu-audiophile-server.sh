aaee6c6350c6d4d08afc7a587ee28de97329752a
a <a>
2021-01-30 01:28:54 +0000

    update

--- ubuntu-audiophile-server.sh
+++ ubuntu-audiophile-server.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# render selection
+echo    "************************"
+echo    "* Select upnp renderer *"
+echo    "************************"
+echo    "* 1. mpd upmpdcli      *"
+echo    "* 2. gmediarender      *"
+echo    "************************"
+echo -n "Select renderer (1/2)   "
+read ans
+
+RENDER=mpd
+[ "$ans" == 2 ] && RENDER=gmediarender
 USERNAME=$(whoami)
 
 # external repos
@@ -12,17 +24,27 @@ sudo apt-get -y dist-upgrade
 # remove useless software
 sudo apt-get -y remove packageit snap update-manager-core
 
-# install software 
-sudo apt-get -y install openssh-server
-sudo apt-get -y install mpd upmpdcli 
-sudo apt-get -y install alsa alsa-tools
+# install system software
 sudo apt-get -y install linux-lowlatency linux-tools-lowlatency cpufrequtils
+sudo apt-get -y install openssh-server mc
+sudo apt-get -y install alsa alsa-tools
+
+# install upnp renderer
+if [ $RENDER == "mpd" ]; then
+  sudo apt-get -y install mpd upmpdcli
+  sudo apt-get -y remove gmediarender gstreamer1.0-alsa gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-tools
+elif [ $RENDER == "gmediarender" ]; then
+  sudo apt-get -y remove mpd upmpdcli
+  sudo apt-get -y install gmediarender gstreamer1.0-alsa gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-tools
+fi
 
 # install configurations
 sudo systemctl disable ondemand
-sudo adduser "$USERNAME" audio
+sudo adduser $USERNAME audio
 sudo cp -r etc/ /
-mkdir -p /home/"$USERNAME"/Music
+sudo sed -i "s/_USERNAME_/$USERNAME/g" /etc/default/gmediarender
+sudo sed -i "s/_RENDER_/$RENDER/g" /etc/systemd/system/rc-local.service
+mkdir -p /home/$USERNAME/Music
 
 # clean up
 sudo apt-get -y autoremove
